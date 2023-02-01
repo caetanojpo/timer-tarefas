2710a96e5686d396949eb12d0ff5f24b4e8e72c3
João Pedro Caetano <caetanojpo@gmail.com>
2023-02-01 10:32:22 -0300

    selecionaTarefa2

--- src/Components/List/Item/index.tsx
+++ src/Components/List/Item/index.tsx
@@ -1,25 +1,33 @@
-import { ITarefa } from '../../../types/tarefa'
-import style from '../List.module.scss'
+import { ITarefa } from "../../../types/tarefa";
+import style from "../List.module.scss";
 
 interface Props extends ITarefa {
-  selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void;
 }
 
-export default function Item ({ tarefa, tempo, selecionado, completado, id, selecionaTarefa } : Props) {
-    return (
-        <li className={style.item} onClick={() => selecionaTarefa({
+export default function Item({
+  tarefa,
+  tempo,
+  selecionado,
+  completado,
+  id,
+  selecionaTarefa,
+}: Props) {
+  return (
+    <li
+      className={style.item}
+      onClick={() =>
+        selecionaTarefa({
           tarefa,
           tempo,
           selecionado,
           completado,
-          id
-        })}>
-                        <h3>
-                          {tarefa}  
-                        </h3>
-                        <span>
-                        {tempo}
-                        </span>
-                    </li>
-    )
+          id,
+        })
+      }
+    >
+      <h3>{tarefa}</h3>
+      <span>{tempo}</span>
+    </li>
+  );
 }
